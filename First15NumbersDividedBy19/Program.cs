3dc229aeed558f8027711c406259de2b32e662ee
исп-22 <-22@LAB30-09>
2023-11-03 11:58:41 +0300

    Добавьте файлы проекта.

---
+++ First15NumbersDividedBy19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int count = 0;
+        int number = 100;
+
+        while (count < 15)
+        {
+            if (number % 19 == 0)
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+
+            number++;
+        }
+    }
+}
