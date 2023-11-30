58b3ec783e210b8296d4772b313fd1f22e44c471
Тогузов М.А <93835834+NuDaYaa@users.noreply.github.com>
2023-11-30 09:35:21 +0300

    Update Program.cs

--- First15NumbersDividedBy19/Program.cs
+++ First15NumbersDividedBy19/Program.cs
@@ -1,21 +1,13 @@
-﻿using System;
+    int count = 0;
+    int number = 100;
 
-class Program
-{
-    static void Main(string[] args)
+    while (count < 15)
     {
-        int count = 0;
-        int number = 100;
-
-        while (count < 15)
+        if (number % 19 == 0)
         {
-            if (number % 19 == 0)
-            {
-                Console.WriteLine(number);
-                count++;
-            }
-
-            number++;
+            Console.WriteLine(number);
+            count++;
         }
+
+        number++;
     }
-}
